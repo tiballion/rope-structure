93f6257e70c90ca8ff4d6fed20320f04c5966b79
thomrenault23 <114226830+thomrenault23@users.noreply.github.com>
2023-10-13 17:23:55 +0200

    Add insertion and deletion

--- main.c
+++ main.c
@@ -4,18 +4,53 @@
 #include "rope.h"
 
 int main() {
-    // Crée un arbre Rope pour la première chaîne
-    Rope *root1 = NULL;
-    char a[] = "111122223333";
-    int n1 = sizeof(a) / sizeof(a[0]) - 1; // Exclut le caractère nul
-    createRopeStructure(&root1, NULL, a, 0, n1 - 1);
+    // Création d'une corde avec une chaîne initiale
+    Rope *root = NULL;
+    char initialString[] = "ABCDEFGHIJ";
+    createRopeStructure(&root, NULL, initialString, 0, strlen(initialString) - 1);
 
-    // Génère le fichier DOT
-    FILE *dotFile = fopen("rope_graph.dot", "w");
+    // Affichage de la corde initiale
+    printf("Initial Rope:\n");
+    printRopeRepresentation(root, 0);
+    printf("String in the Rope:\n");
+    printString(root);
+    printf("\n\n");
+
+    // Test de la fonction d'insertion à un index spécifié
+    int indexToInsert = 4;
+    char stringToInsert[] = "O";
+    printf("Inserting \"%s\" at index %d:\n", stringToInsert, indexToInsert);
+    insertAtIndex(&root, indexToInsert, stringToInsert);
+
+    // Affichage de la corde après l'insertion
+    printf("Rope after insertion:\n");
+    printRopeRepresentation(root, 0);
+    printf("String in the Rope:\n");
+    printString(root);
+    printf("\n");
+
+    // Test de la fonction de suppression à un index spécifié
+    int indexToDelete = 6;
+    int lengthToDelete = 3; // Longueur de la plage à supprimer
+    printf("Deleting character at index %d:\n", indexToDelete);
+    deleteAtIndex(&root, indexToDelete, lengthToDelete);
+
+    // Affichage de la corde après la suppression
+    printf("Rope after deletion:\n");
+    printRopeRepresentation(root, 0);
+    printf("String in the Rope:\n");
+    printString(root);
+    printf("\n");
+
+    // Génère à nouveau le fichier DOT pour la corde mise à jour
+    FILE *dotFile = fopen("rope_graph_after_insert.dot", "w");
     fprintf(dotFile, "digraph Rope {\n");
-    generateDotCode(root1, dotFile);
+    generateDotCode(root, dotFile);
     fprintf(dotFile, "}\n");
     fclose(dotFile);
+
+    // Libération de la mémoire allouée pour la corde
+    freeRope(root);
 
     return 0;
 }
